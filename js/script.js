ecff637e960da5da94732040b07c12dab1155298
Mark Whitaker <mark@mainwave.co.uk>
2020-05-17 15:04:00 +0100

    Add flags (#3)
    * Added flag to modal dialog header
    
    * Tweaked spacing between flag and country name
    
    * Added event handler to hide the flag if it fails to load
    
    * Fixed Scotland country code so flag will display

--- js/script.js
+++ js/script.js
@@ -52,6 +52,15 @@ $(function () {
         $("#btnShowList").click(function () {
             showList();
         });
+
+        $('#filmCountryFlag').on({
+            "error": function () {
+                $(this).hide();
+            },
+            "load": function () {
+                $(this).show();
+            }
+        });
     }
 
     function loadData() {
@@ -128,6 +137,9 @@ $(function () {
         $("#filmCountry").text(film.country);
         $("#filmTitle").text(film.title);
         $("#filmYear").text(film.year);
+        $("#filmCountryFlag")
+            .prop("src", "https://flagpedia.net/data/flags/vector/" + film.countryCode.toLowerCase() + ".svg")
+            .prop("alt", "National flag of " + film.country);
 
         if (film.image) {
             $("#filmImageContainer").removeClass("defaultImage");
